1b5b059eec6257a978910189d5e12d3952270146
Solon Aguiar <solon.aguiar@checkr.com>
2022-12-20 16:41:19 -0800

    more indirection

---
+++ bin/generate_chart.sh
@@ -0,0 +1,1 @@
+echo "apiVersion: $CI_CHART_API_VERSION\ndescription: Chart for deployment\nname: checkr-embed-patterns\nversion: $CI_REPO_VERSION\ndependencies:\n  - name: $CI_CHART_NAME\n    version: $CI_CHART_VERSION\n    repository: $CI_CHART_REPOSITORY\n    alias: $CI_CHART_ALIAS\n" > .gitops/helm/checkr-embed-patterns/Chart.yaml

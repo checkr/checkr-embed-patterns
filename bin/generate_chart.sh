b44736d3a3eb0254b76d90f964b87bb3bcd04449
Solon Aguiar <solon.aguiar@checkr.com>
2023-11-30 13:19:48 -0800

    Use sandbox

--- bin/generate_chart.sh
+++ bin/generate_chart.sh
@@ -10,26 +10,23 @@ printf "  version: $CHART_VERSION\n" >> .gitops/helm/checkr-embed-patterns/Chart
 printf "  repository: $CHART_REPOSITORY\n" >> .gitops/helm/checkr-embed-patterns/Chart.yaml
 printf "  alias: $CHART_ALIAS\n" >> .gitops/helm/checkr-embed-patterns/Chart.yaml
 
-printf "microservice:\n" > .gitops/helm/checkr-embed-patterns/production.yaml
-printf "  environment: production\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "  nameOverride: checkr-embed-patterns\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "  deployments:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      command: ['node', 'app.js']\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      replicaCount: $REPLICA_COUNT\n\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "  ingresses:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      type: $INGRESS_TYPE\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      hosts:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "        - host: $INGRESS_HOSTS\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "          serviceName: web\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "          servicePort: 80\n\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "  services:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      ports:\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "        - port: 80\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "          targetPort: 5000\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-printf "      selector: web\n" >> .gitops/helm/checkr-embed-patterns/production.yaml
-
-echo "production"
-cat .gitops/helm/checkr-embed-patterns/production.yaml
+printf "microservice:\n" > .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "  environment: sandbox\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "  nameOverride: checkr-embed-patterns\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "  deployments:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      command: ['node', 'app.js']\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      replicaCount: $REPLICA_COUNT\n\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "  ingresses:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      type: $INGRESS_TYPE\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      hosts:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "        - host: $INGRESS_HOSTS\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "          serviceName: web\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "          servicePort: 80\n\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "  services:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "    web:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      ports:\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "        - port: 80\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "          targetPort: 5000\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
+printf "      selector: web\n" >> .gitops/helm/checkr-embed-patterns/sandbox_us.yaml
